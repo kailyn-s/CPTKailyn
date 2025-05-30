de6dea1380d77bface9bbdf510fa2fdbec7ce6ea
kailyn <kailyn.sy26@ycdsbk12.ca>
2025-05-30 10:27:49 -0400

    created main menu file and basic skeleton

---
+++ CPTPokerMain.java
@@ -0,0 +1,13 @@
+import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage; 
+
+public class CPTPokerMain{
+	public static void main(String[] args){
+		Console con = new Console("Poker - ICS3U1 CPT", 1280, 720);
+		CPTMainMenu.MainMenu(con);
+		// TEST
+		
+	
+	}
+}
