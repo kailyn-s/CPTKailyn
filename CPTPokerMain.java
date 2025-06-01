9db482107e190bee3ddf2accadfa75f2317c222a
kailyn <kailyn.sy26@ycdsbk12.ca>
2025-06-01 15:26:52 -0400

    created main menu with options play, help, leaderboard, quit

--- CPTPokerMain.java
+++ CPTPokerMain.java
@@ -5,8 +5,24 @@ import java.awt.image.BufferedImage;
 public class CPTPokerMain{
 	public static void main(String[] args){
 		Console con = new Console("Poker - ICS3U1 CPT", 1280, 720);
-		CPTMainMenu.MainMenu(con);
-		// TEST
+		CPTtools.mainmenu(con);
+		
+		char chrChoice; 
+		chrChoice = con.getChar();
+		
+		if(chrChoice == 'p'){
+			CPTtools.clear(con);
+		}else if(chrChoice == 'h'){
+			CPTtools.clear(con);
+		}else if(chrChoice == 'l'){
+			CPTtools.clear(con);
+		}else if(chrChoice == 'q'){
+			CPTtools.clear(con);
+		}
+			
+		
+		
+		
 		
 	
 	}
---
+++ CPTtools.java
@@ -0,0 +1,31 @@
+import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage; 
+
+public class CPTtools{
+	public static void main(String[] args){
+		
+	}
+	
+	public static void mainmenu(Console con){
+		BufferedImage imgMainMenu = con.loadImage("mainmenu.png");
+		con.drawImage(imgMainMenu, 0, 0);
+		con.println("");
+	}
+	
+	public static void clear(Console con){
+		con.clear();
+		con.setDrawColor(Color.BLACK);
+		con.fillRect(0, 0, 1280, 720);
+		BufferedImage imgMoney = con.loadImage("money.png");
+		con.drawImage(imgMoney, 0, 0);
+		con.println(" ");
+		
+	}
+	
+	
+	
+	
+
+	
+}
