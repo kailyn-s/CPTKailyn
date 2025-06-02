e5b4b4be8b6db7fdfff7d0b4ee17aec040f4d670
kailyn <kailyn.sy26@ycdsbk12.ca>
2025-06-01 23:48:51 -0400

    created card and suit column of 52x3 array

--- CPTPokerMain.java
+++ CPTPokerMain.java
@@ -9,10 +9,30 @@ public class CPTPokerMain{
         
         String strName;
 		char chrChoice; 
-		chrChoice = con.getChar();
 		int intMoney = 1000;
 		int intBet; 
+		int intCount;
+		String strCards[][];
 		
+		strCards = new String[52][3];
+		
+		//Column 0 - Card Number
+		for(intCount = 0; intCount < 52; intCount++){
+			strCards[intCount][0] = ""+ (intCount % 13 + 1);
+		}
+		
+		//Column 1 - Suit
+		/*Diamonds - 1
+		 * Clubs - 2
+		 * Hearts - 3
+		 * Spades - 4 */
+		 
+		for(intCount = 0; intCount < 52; intCount++){
+			strCards[intCount][1] = "" + (int)(intCount/13 + 1);		
+		}
+		
+		chrChoice = con.getChar();
+		//Play Option
 		if(chrChoice == 'p'){
 			CPTtools.clear(con);
 			CPTtools.getname(con, intMoney);
@@ -23,10 +43,16 @@ public class CPTPokerMain{
 			CPTtools.clear(con);
 			intMoney = CPTtools.calculatemoney(intBet, intMoney);
 			CPTtools.printmoney(con, intMoney);
+			
+		//Help Option
 		}else if(chrChoice == 'h'){
 			CPTtools.help(con);
+			
+		//Leaderboard Option
 		}else if(chrChoice == 'l'){
 			CPTtools.clear(con);
+			
+		//Quit Option
 		}else if(chrChoice == 'q'){
 			CPTtools.clear(con);
 		}
